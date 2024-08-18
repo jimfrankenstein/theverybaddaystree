dce5f9affeb4d05858b6f8999a7b6ae8161c5366
Brian Reed <brian.reed.design@gmail.com>
2024-08-18 14:37:47 -0500

    Fix event on track pages

--- assets/js/components/events.js
+++ assets/js/components/events.js
@@ -1,11 +1,12 @@
-// facebook spotify event
-document.querySelectorAll('[data-fbtrack]').forEach(function(el) {
-    el.addEventListener('click', function(e) {
-        e.preventDefault();
-        var trackName = this.getAttribute('data-fbtrack');
-        fbq('trackCustom', 'LinkClickToSpotify_' + trackName);
-        setTimeout(function() {
-            window.location.href = this.getAttribute('href');
-        }, 500);
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('[data-fbtrack]').forEach(function(el) {
+        el.addEventListener('click', function(e) {
+            e.preventDefault();
+            var trackName = this.dataset.fbtrack;
+            fbq('trackCustom', 'LinkClickToSpotify_' + trackName);
+            setTimeout(function() {
+                window.location.href = el.href;
+            }, 500);
+        });
     });
 });
