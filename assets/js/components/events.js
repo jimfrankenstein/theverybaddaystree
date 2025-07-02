18272fa2016709a4e2d8c555601c9dd383dede86
Brian Reed <brian.reed.design@gmail.com>
2025-07-02 10:03:58 -0500

    Revert "Update tracking for GA"
    This reverts commit 58549d9367f286c626fe13d533e73650dfe3220b.

--- assets/js/components/events.js
+++ assets/js/components/events.js
@@ -1,44 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Handle music platform link tracking
-    document.querySelectorAll('[data-track-platform]').forEach(function(el) {
-        el.addEventListener('click', function(e) {
-            e.preventDefault();
-            var url = this.href;
-
-            var platform = this.dataset.trackPlatform;
-            platform = platform.charAt(0).toUpperCase() + platform.slice(1);
-            var songName = this.dataset.song;
-            var trackingLabel = platform + ' for ' + songName;
-
-            console.debug('Clicked: ' + trackingLabel);
-            
-            // Facebook Pixel tracking
-            if (typeof fbq !== 'undefined') {
-                var fbqEvent = 'LinkClickTo' + platform;
-                fbq('trackCustom', fbqEvent);
-                console.debug(fbqEvent);
-            }
-            
-            // Google Analytics tracking
-            if (typeof gtag !== 'undefined') {
-                gtag('event', 'music_link_click', {
-                    event_category: 'Outbound Link',
-                    event_label: 'Platform Link Click',
-                    song_name: songName,
-                    platform: platform,
-                  });
-                console.debug('GA ' + trackingLabel);
-            }
-            console.debug('END');
-
-            // Give GA time to send the event
-            setTimeout(function() {
-              window.location.href = url;
-            }, 200);
-        });
-    });
-
-    // Legacy support for existing data-fbtrack elements
     document.querySelectorAll('[data-fbtrack]').forEach(function(el) {
         el.addEventListener('click', function(e) {
             e.preventDefault();
